978c8e66c474fb28cbdf417d9a469cdebd9a7505
arockiyajebasanthiya <arockiyajebasanthiya@gmail.com>
2025-05-05 12:03:46 +0530

    commit changes

--- AutomaticCodeCopy.py
+++ AutomaticCodeCopy.py
@@ -92,6 +92,13 @@ import urllib.parse
 import sys
 import os
 import dropbox
+from configparser import Error
+import dropbox.exceptions
+import samplecode
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError,AuthError
+
 APP_KEY = 'fwoh8e8aey34oto'
 APP_SECRET = 'ggiqq9eh4n1tenp'
 REDIRECT_URI = 'http://localhost:8080'
@@ -192,7 +199,62 @@ def get_tokens(auth_code=None):
             print(response.text)
             return None
 
+def create_folder_if_not_exists(folder_path):
+    try:
+        dbx.files_get_metadata(folder_path)
+        print("Folder already present")
+    except ApiError as e:
+        if isinstance(e.error, dropbox.files.GetMetadataError) and e.error.is_path() and e.error.get_path().is_not_found():
+            dbx.files_create_folder_v2(folder_path)
+            print("Folder created")
+def upload_file_in_folder(file_path,dropbox_folder_path):
+    dropbox_files_path = f"{dropbox_folder_path}/{os.path.basename(file_path)}"
+    print("folder path:", dropbox_files_path)
+    try:
+        with open(file_path , "rb") as f:
+            dbx.files_upload(f.read(),dropbox_files_path,mode=WriteMode('overwrite'))
+            print("File uploaded")
+    except ApiError as err:
+            if (err.error.get_path() and err.error.get_path().error.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+        
+def search_dropbox(filename):
+    try:
+        results = dbx.files_search('', filename)
+        list=[]
+        if results.matches:
+            print("Search results:")
+            for match in results.matches:
+                if isinstance(match, dropbox.files.SearchMatch):
+                    print(f"- {match.metadata.path_display}")
+                    list.append(match.metadata.path_display)
+            return list           
+        else:
+            print(f"No files found matching '{filename}'.")
+            return null
 
+    except dropbox.exceptions.ApiError as err:
+        print(f"Dropbox API error: {err}") 
+        
+def download_files(dropbox_path):
+    try:
+        current_dir = os.getcwd()
+        relative_path = dropbox_path.lstrip("/")
+        local_file_path = os.path.join(current_dir, relative_path)
+
+        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+        with open(local_file_path,'wb') as f:
+            metadata,res=dbx.files_download(path=dropbox_path)
+            f.write(res.content)
+            print("Files downloaded")
+    except ApiError as e:
+        print(f"Error :{e}")
 if __name__ == '__main__':
 
     tokens = get_tokens()
@@ -211,4 +273,19 @@ if __name__ == '__main__':
     print(tokens.get('access_token'))
     access_token=tokens.get('access_token')
     dbx=dropbox.Dropbox(access_token)
+    folder_path=input("Enter the folder name to add your file(if the folder is missing then it create one):")
+    if not folder_path.startswith('/'):
+        folder_path = '/' + folder_path
+    create_folder_if_not_exists(folder_path)
+    local_path=input("Enter your entire file path:")
+    if os.path.isfile(local_path):
+        print("File present in it")
+    else:
+        print("File not present")
+        sys.exit(1)
+    upload_file_in_folder(local_path,folder_path)
+    filename=input("Enter file name to search:")
+    results=search_dropbox(filename)
+    print(results)
+    download_files(results[0])
     print("Done")
